fc738e2b69ffe87ee15d02bbc26dd912f053eba2
Sanjay Krishna Anbalagan <sanjay@iweave.com>
2017-04-11 08:59:23 -0400

    eslint config file added to postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -27,10 +27,12 @@ command.createDir( '../../src',function(){
             command.copyDir( './scripts', '../../scripts',function(){
                 command.copyFile( './webpack.config.js', '../../webpack.config.js',function(){
                     command.copyFile( './.babelrc', '../../.babelrc',function(){
-                        command.updateJson( '../../package.json', newJson,function(){
-                            command.remove('../../scripts/postinstall.js',function(){
-                                command.remove('../../node_modules/library-boilerplate')
-                            })
+                        command.copyFile( './.eslintrc', '../../.eslintrc',function(){
+                            command.updateJson( '../../package.json', newJson,function(){
+                                command.remove('../../scripts/postinstall.js',function(){
+                                    command.remove('../../node_modules/library-boilerplate')
+                                })
+                            });
                         });
                     });
                 });
